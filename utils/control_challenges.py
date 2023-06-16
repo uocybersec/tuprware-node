47bcdbe4ba4104a879be1a6281e3b7d7e5680039
Ryan Awad <ryanawad@yahoo.com>
2023-06-15 22:58:06 -0400

    Added host port selection algorithm

--- utils/control_challenges.py
+++ utils/control_challenges.py
@@ -1,21 +1,24 @@
 import docker
+from utils.manage_host_ports import get_host_port
 
 CONTAINER_PORT = 1337
+CONTAINER_ALLOCATED_MEMORY = "128m" # every challenge container is allocated 128 megabytes of memory
+
 client = docker.from_env()
 
-def spawn_challenge(challenge_id, instance_id):
-    memory_allocated = "128m" # fetch it from the specific challenge
+def spawn_challenge(challenge_id: str, instance_id: str) -> None:
     # ALSO FIND A WAY TO LOG SOMEWHERE THAT A CONTAINER RAN OUT OF MEMORY SO WE KNOW WHAT CAUSES ISSUES DURING THE CTF
+    host_port = get_host_port()
     client.containers.run(
         image=f'uoctf-{challenge_id}', 
         name=f'uoctf-{challenge_id}-{instance_id}', 
         detach=True, 
-        ports={CONTAINER_PORT: 7000}, # HANDLE HOST PORT SELECTION,
-        mem_limit=memory_allocated
+        ports={CONTAINER_PORT: host_port},
+        mem_limit=CONTAINER_ALLOCATED_MEMORY # memory is not being allocated to containers when ran on my laptop. this is an issue with my laptop's install of docker. but its important to note that this issue could also happen on nodes. 
     )
 
 
-def stop_challenge(challenge_id, instance_id):
+def stop_challenge(challenge_id: str, instance_id: str) -> None:
     target_container = client.containers.get(
         container_id=f'uoctf-{challenge_id}-{instance_id}'
     )
@@ -23,6 +26,6 @@ def stop_challenge(challenge_id, instance_id):
     target_container.remove()
 
 
-def restart_challenge(challenge_id, instance_id):
+def restart_challenge(challenge_id: str, instance_id: str) -> None:
     stop_challenge(challenge_id, instance_id)
     spawn_challenge(challenge_id, instance_id)
--- utils/manage_host_ports.py
+++ utils/manage_host_ports.py
@@ -1,3 +1,18 @@
-# used to manage the whole ordeal of mapping the container ports to real ports on the host machine without clashing with anyone else's instances
+import psutil
+from random import randint
 
-# MAKE SURE TO NEVER USE UNSAFE PORTS ON CHROME (ex: port 6000 is considered an unsafe port on chrome)
+MIN_PORT = 1024
+MAX_PORT = 65535
+
+def is_port_in_use(port: int) -> bool:
+    for conn in psutil.net_connections(): # check connections on ports over TCP and UDP for both IPv4 and IPv6
+        if conn.laddr.port == port:
+            return True
+    return False
+
+def get_host_port() -> int: # returns a random available host port
+    while True:
+        port = randint(MIN_PORT, MAX_PORT)
+        if not is_port_in_use(port):
+            return port
+    
