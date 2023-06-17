ab48fab695048abc776388084229e589bbb49914
Ryan Awad <ryanawad@yahoo.com>
2023-06-16 23:40:28 -0400

    added redis

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
-
 # USEFUL REDDIT THREAD: https://www.reddit.com/r/docker/comments/u73dxw/starting_containers_dynamically_question/
 
-from flask import Flask, request
+from flask import Flask
+from flask import request
+from flask_jwt_extended import JWTManager
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import jwt_required
 import docker
-from utils.control_challenges import spawn_challenge, stop_challenge, restart_challenge
+import redis
 from time import time
+from utils.control_challenges import spawn_challenge
+from utils.control_challenges import stop_challenge
+from utils.control_challenges import restart_challenge
 
 app = Flask(__name__)
+app.config['JWT_SECRET_KEY'] = 'test_secret_key' # CHANGE THIS !!!
+
+jwt = JWTManager(app)
 docker_client = docker.from_env()
+redis_conn = redis.Redis(
+    host='localhost', 
+    port=6379, 
+    decode_responses=True
+)
 
 # ********************** ADD AUTHENTICATION ********************************
 @app.route('/spawn-challenge', methods=["POST"])
@@ -16,7 +31,9 @@ def spawn():
     instance_id = time() # figure out new way to generate instance ID. Ideas: UNIX timestamp, username or handle in the CTF, teamname, etc.
     spawn_challenge(
         challenge_id=challenge_id,
-        instance_id=instance_id
+        instance_id=instance_id,
+        redis_conn=redis_conn,
+        user_id="test"
     )
     return "Challenge instance spawned.", 200
 
@@ -27,7 +44,9 @@ def stop():
     instance_id = request.json['instance_id']
     stop_challenge(
         challenge_id=challenge_id,
-        instance_id=instance_id
+        instance_id=instance_id,
+        redis_conn=redis_conn,
+        user_id="test"
     )
     return "Challenge instance stopped.", 200
 
@@ -38,10 +57,19 @@ def restart():
     instance_id = request.json['instance_id']
     restart_challenge(
         challenge_id=challenge_id,
-        instance_id=instance_id
+        instance_id=instance_id,
+        redis_conn=redis_conn,
+        user_id="test"
     )
     return "Challenge instance restarted.", 200
 
+@app.route('/get-node-available-memory', methods=["GET"])
+def get_avail_mem():
+    return "work in progress", 200
+
+@app.route('/test')
+def test():
+    return create_access_token(identity='e0df7f84-0061-44d3-b531-e4bc22428a27')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', debug=False)
--- utils/control_challenges.py
+++ utils/control_challenges.py
@@ -1,4 +1,5 @@
 import docker
+import redis
 from utils.manage_host_ports import get_host_port
 
 CONTAINER_PORT = 1337
@@ -6,26 +7,29 @@ CONTAINER_ALLOCATED_MEMORY = "128m" # every challenge container is allocated 128
 
 client = docker.from_env()
 
-def spawn_challenge(challenge_id: str, instance_id: str) -> None:
+def spawn_challenge(challenge_id: str, instance_id: str, redis_conn: redis.Redis, user_id: str) -> None:
     # ALSO FIND A WAY TO LOG SOMEWHERE THAT A CONTAINER RAN OUT OF MEMORY SO WE KNOW WHAT CAUSES ISSUES DURING THE CTF
     host_port = get_host_port()
+    instance_name = f'uoctf-{challenge_id}-{instance_id}'
     client.containers.run(
         image=f'uoctf-{challenge_id}', 
-        name=f'uoctf-{challenge_id}-{instance_id}', 
+        name=instance_name, 
         detach=True, 
         ports={CONTAINER_PORT: host_port},
         mem_limit=CONTAINER_ALLOCATED_MEMORY # memory is not being allocated to containers when ran on my laptop. this is an issue with my laptop's install of docker. but its important to note that this issue could also happen on nodes. 
     )
+    redis_conn.set(user_id, instance_name)
 
 
-def stop_challenge(challenge_id: str, instance_id: str) -> None:
+def stop_challenge(challenge_id: str, instance_id: str, redis_conn: redis.Redis, user_id: str) -> None:
     target_container = client.containers.get(
         container_id=f'uoctf-{challenge_id}-{instance_id}'
     )
     target_container.stop()
     target_container.remove()
+    redis_conn.delete(user_id)
 
 
-def restart_challenge(challenge_id: str, instance_id: str) -> None:
-    stop_challenge(challenge_id, instance_id)
-    spawn_challenge(challenge_id, instance_id)
+def restart_challenge(challenge_id: str, instance_id: str, redis_conn: redis.Redis, user_id: str) -> None:
+    stop_challenge(challenge_id, instance_id, redis_conn, user_id)
+    spawn_challenge(challenge_id, instance_id, redis_conn, user_id)
