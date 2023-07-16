366fde84ee6b8d7fb37f81002bd949b7dca99fea
ryan-awad <ryanawad@yahoo.com>
2023-07-16 16:27:59 +0000

    connected tuprware to the db lambda

--- utils/control_challenges.py
+++ utils/control_challenges.py
@@ -3,44 +3,46 @@ import redis
 import json
 from utils.manage_host_ports import get_host_port
 from typing import Tuple
+from db_operations import get_running_instance_id
+from db_operations import set_running_instance_id
 
 CONTAINER_PORT = 1337
 CONTAINER_ALLOCATED_MEMORY = "128m" # every challenge container is allocated 128 megabytes of memory
 
 client = docker.from_env()
 
-def spawn_challenge(challenge_id: str, instance_id: str, redis_conn: redis.Redis, user_id: str) -> None:
+def spawn_challenge(challenge_id: str, instance_id: str, user_id: str) -> None:
     # ALSO FIND A WAY TO LOG SOMEWHERE THAT A CONTAINER RAN OUT OF MEMORY SO WE KNOW WHAT CAUSES ISSUES DURING THE CTF
-    if redis_conn.get(user_id) == None: # check if the user does not already have an instance running
+    if get_running_instance_id(user_id) == None: # check if the user does not already have an instance running
         with open('challenges.json', mode='r') as challenges_json:
             challenges = json.loads(challenges_json.read())
             if challenge_id in challenges.keys():
                 host_port = get_host_port()
-                instance_name = f'uoctf-{challenge_id}-{instance_id}'
+                instance_id = f'uoctf-{challenge_id}-{instance_id}'
                 client.containers.run(
                     image=challenges[challenge_id], 
-                    name=instance_name, 
+                    name=instance_id, 
                     detach=True, 
                     ports={CONTAINER_PORT: host_port},
                     mem_limit=CONTAINER_ALLOCATED_MEMORY # memory is not being allocated to containers when ran on my laptop. this is an issue with my laptop's install of docker. but its important to note that this issue could also happen on nodes. 
                 )
-                redis_conn.set(user_id, instance_name)
+                set_running_instance_id(user_id, instance_id)
             else:
                 raise Exception("Invalid challenge ID.")
     else: # if the user already has an instance running, shutdown the instance and start this one
-        stop_challenge(redis_conn, user_id)
+        stop_challenge(user_id)
         spawn_challenge(challenge_id, instance_id, redis_conn, user_id)
 
 
-def stop_challenge(redis_conn: redis.Redis, user_id: str) -> Tuple[str, str]:
-    container_id = redis_conn.get(user_id)
+def stop_challenge(user_id: str) -> Tuple[str, str]:
+    container_id = get_running_instance_id(user_id)
     if container_id:
         target_container = client.containers.get(
             container_id=container_id
         )
         target_container.stop()
         target_container.remove()
-        redis_conn.delete(user_id)
+        redis_conn.delete(user_id) # CONTINUE HERE . . . . . . . .
         _, challenge_id, instance_id = container_id.split('-')
         return challenge_id, instance_id
     else:
