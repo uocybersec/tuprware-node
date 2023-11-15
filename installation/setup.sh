e794aa2fb635bc0f4eae699d016a9fdfde576c37
Ubuntu <ubuntu@ip-172-31-0-64.ec2.internal>
2023-11-15 00:08:20 +0000

    using nginx reverse proxy with subdomains

--- installation/setup.sh
+++ installation/setup.sh
@@ -7,28 +7,68 @@ if [[ $(groups $USER) == *"docker"* ]]; then
     sudo apt-get update -y
     sudo apt-get install python3-pip -y
     sudo apt-get install docker.io -y
-    sudo apt install apache2 -y
+    sudo apt install nginx -y
     sudo apt install gunicorn -y
 
-    if test ! -f .env; then
+    if test ! -f ../.env; then
         echo -e "\n\nCreating .env file";
-        echo "JWT_SECRET_KEY=$(openssl rand -hex 32)" > .env
+        echo "JWT_SECRET_KEY=$(openssl rand -hex 32)" > ../.env
         echo "Tuprware Node needs an IAM user in AWS to access certain resources. Enter the following credentials for this IAM user.";
         read -p "Access Key: " access_key
         read -p "Secret Key: " secret_key
         read -p "AWS S3 bucket name for storing the challenges: " challenge_bucket_name
-        echo "AWS_ACCESS_KEY=$access_key" >> .env
-        echo "AWS_SECRET_KEY=$secret_key" >> .env
-        echo "AWS_CHALLENGE_S3_BUCKET_NAME=$challenge_bucket_name" >> .env
+        echo "AWS_ACCESS_KEY=$access_key" >> ../.env
+        echo "AWS_SECRET_KEY=$secret_key" >> ../.env
+        echo "AWS_CHALLENGE_S3_BUCKET_NAME=$challenge_bucket_name" >> ../.env
     fi  
 
-    pip3 install -r requirements.txt
+    pip3 install -r ../requirements.txt
     rm -rf /tmp/uoctf-challenges
     mkdir /tmp/uoctf-challenges
     python3 build_challenges.py
 
-    sudo sh -c 'cat apache2.conf > /etc/apache2/apache2.conf'
-    sudo systemctl restart apache2
+    sudo rm -rf /etc/nginx/sites-available/default
+    sudo rm -rf /etc/nginx/sites-enabled/default
+    sudo sh -c 'cat <<EOF > /etc/nginx/sites-available/tuprware
+server {
+    listen 80;
+    server_name ctf.uocybersec.lol;
+
+    location / {
+            include proxy_params;
+            proxy_pass http://unix:/tuprware-node/app.sock;
+    }
+}
+
+server {
+    listen 80;
+    server_name ~^(?<subdomain>\w+)\.uocybersec\.lol$;
+
+    location / {
+        set $port "";
+
+        # Extract the port number from the subdomain
+        if ($subdomain ~ ^(?<port>\d+)$) {
+            set $port $subdomain;
+        }
+
+        # Deny access to specific ports
+        # to block multiple ports, do <port>|<port>
+        # MAKE SURE TO BLOCK containerd PORT (use netstat -nl to find it)
+        if ($port ~ ^(22)$) {
+            return 403;  # Forbidden
+        }
+
+        proxy_pass http://127.0.0.1:$port;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+    }
+}
+EOF'
+    sudo ln -s /etc/nginx/sites-available/tuprware /etc/nginx/sites-enabled/
+    sudo systemctl restart nginx
 else
     sudo apt-get update -y
     sudo apt-get install docker.io -y
